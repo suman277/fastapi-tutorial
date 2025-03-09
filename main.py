0f539d93f64dfca3da544ba09abfa7942e69456a
Suman Kumar Sahu <sumankumarsahu@Sumans-MacBook-Air.local>
2025-03-10 00:07:08 +0530

    Learnt Query Parameter

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 from fastapi import FastAPI
 
-app=FastAPI()
+app=FastAPI() #creating an instance of FastAPI
 
-@app.get("/")
+@app.get("/") #creating a route
 def hoome():
     return "Home Page"
 
-print("Hello World")
+@app.get("/json") #creating a route
+def json_data():
+    return {"data": " FastAPI is cool"}
+
+@app.get("/about")
+def about_page():
+    return {"about": "This is a about page"}
+
+# Path Parameters
+@app.get('/blogs')
+def get_blogs():
+    return {"data" :[{"blogs": "1"}, {"blogs": "2"}]}
+
+@app.get('/blogs/{id}')
+def get_id(id:int):
+    return {"data": id}
+
+@app.get("/blog/{id}/comments")
+def get_comments(id:int):
+    return {"data": id}
+
+# always keep the dyanmic routes at the end of the static routes
